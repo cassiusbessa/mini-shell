1ba649162c49252ad1b2000044659359819fc715
cassius <cassiusbessa@gmail.com>
2024-10-07 17:53:17 -0300

    norm fix

--- args.c
+++ args.c
@@ -1,28 +1,28 @@
 #include "minishell.h"
 
-char  **build_args(t_token_lst l)
+char	**build_args(t_token_lst l)
 {
-  t_token *tmp;
-  int     i;
-  char    **args;
+	t_token	*tmp;
+	int			i;
+	char		**args;
 
-  i = 0;
-  tmp = l.head;
-  while (tmp && tmp->type != PIPE )
-  {
-    if (tmp->type == COMMAND || tmp->type == ARGUMMENT)
-      i++;
-    tmp = tmp->next;
-  }
-  args = malloc(sizeof(char *) * (i + 1));
-  i = 0;
-  tmp = l.head;
-  while (tmp && tmp->type != PIPE)
-  {
-    if (tmp->type == COMMAND || tmp->type == ARGUMMENT)
-      args[i++] = tmp->word;
-    tmp = tmp->next;
-  }
-  args[i] = NULL;
-  return (args);
+	i = 0;
+	tmp = l.head;
+	while (tmp && tmp->type != PIPE)
+	{
+		if (tmp->type == COMMAND || tmp->type == ARGUMMENT)
+			i++;
+		tmp = tmp->next;
+	}
+	args = malloc(sizeof(char *) * (i + 1));
+	i = 0;
+	tmp = l.head;
+	while (tmp && tmp->type != PIPE)
+	{
+		if (tmp->type == COMMAND || tmp->type == ARGUMMENT)
+			args[i++] = tmp->word;
+		tmp = tmp->next;
+	}
+	args[i] = NULL;
+	return (args);
 }
--- env.c
+++ env.c
@@ -1,91 +1,90 @@
 #include "minishell.h"
 
-t_hashmap   *build_envs(char **envp);
-static int  find_equals(char *s);
-static char *build_line(t_key_value *kv);
-char        **to_envp(t_hashmap map);
+t_hashmap		*build_envs(char **envp);
+static int	find_equals(char *s);
+static char	*build_line(t_key_value *kv);
+char				**to_envp(t_hashmap map);
 
-t_hashmap   *build_envs(char **envp)
+t_hashmap	*build_envs(char **envp)
 {
-    int         i;
-    t_hashmap   *envs;
-    char        *k;
-    char        *v;
+	int				i;
+	t_hashmap	*envs;
+	char			*k;
+	char			*v;
 
-    envs = create_hash_map();
-    i = 0;
-    k = NULL;
-    v = NULL;
-    while (*envp)
-    {
-        i = find_equals(*envp);
-        k = ft_substr(*envp, 0, i);
-        v = ft_substr(*envp, i + 1, ft_strlen(*envp) - (i + 1));
-        insert_pair(&envs, create_pair(k, v));
-        free(k);
-        free(v);
-        envp++;
-    }
-    return (envs);
+	envs = create_hash_map();
+	i = 0;
+	k = NULL;
+	v = NULL;
+	while (*envp)
+	{
+		i = find_equals(*envp);
+		k = ft_substr(*envp, 0, i);
+		v = ft_substr(*envp, i + 1, ft_strlen(*envp) - (i + 1));
+		insert_pair(&envs, create_pair(k, v));
+		free(k);
+		free(v);
+		envp++;
+	}
+	return (envs);
 }
 
-static int  find_equals(char *s)
+static int	find_equals(char *s)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == '=')
-            return (i);
-        i++;
-    }
-    return (i);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '=')
+			return (i);
+		i++;
+	}
+	return (i);
 }
 
-static char *build_line(t_key_value *kv)
+static char	*build_line(t_key_value *kv)
 {
-    char    *env;
+	char	*env;
 
-    env = NULL;
-
-    if (kv->value)
-    {
-        env = ft_calloc(ft_strlen(kv->key) + ft_strlen(kv->value) + 2, sizeof(char));
-
-        ft_strlcpy(env, kv->key, ft_strlen(kv->key) + 1);
-        ft_strlcat(env, "=", ft_strlen(env) + 2);
-        ft_strlcat(env, kv->value, ft_strlen(env) + ft_strlen(kv->value) + 1);
-    }
-    else
-    {
-        env = ft_calloc(ft_strlen(kv->key) + 2, sizeof(char));
-        ft_strlcpy(env, kv->key, ft_strlen(kv->key) + 1);
-        ft_strlcat(env, "=", ft_strlen(env) + 2);
-    }
-    return (env);
+	env = NULL;
+	if (kv->value)
+	{
+		env = ft_calloc(ft_strlen(kv->key)
+				+ ft_strlen(kv->value) + 2, sizeof(char));
+		ft_strlcpy(env, kv->key, ft_strlen(kv->key) + 1);
+		ft_strlcat(env, "=", ft_strlen(env) + 2);
+		ft_strlcat(env, kv->value, ft_strlen(env) + ft_strlen(kv->value) + 1);
+	}
+	else
+	{
+		env = ft_calloc(ft_strlen(kv->key) + 2, sizeof(char));
+		ft_strlcpy(env, kv->key, ft_strlen(kv->key) + 1);
+		ft_strlcat(env, "=", ft_strlen(env) + 2);
+	}
+	return (env);
 }
 
-char    **to_envp(t_hashmap map)
+char	**to_envp(t_hashmap map)
 {
-    char    **envp;
-    t_part     i;
-    t_key_value *current;
+	char				**envp;
+	t_part			i;
+	t_key_value	*current;
 
-    current = NULL;
-    envp = (char **)ft_calloc(map.size + 1, sizeof(char *));
-    i.start = 0;
-    i.end = 0;
-    while (i.start < TABLE_SIZE)
-    {
-        current = map.table[i.start];
-        while (current)
-        {
-            envp[i.end++] = build_line(current);
-            current = current->next;
-        }
-        i.start++;
-    }
-    envp[i.end] = NULL;
-    return (envp);
+	current = NULL;
+	envp = (char **)ft_calloc(map.size + 1, sizeof(char *));
+	i.start = 0;
+	i.end = 0;
+	while (i.start < TABLE_SIZE)
+	{
+		current = map.table[i.start];
+		while (current)
+		{
+			envp[i.end++] = build_line(current);
+			current = current->next;
+		}
+		i.start++;
+	}
+	envp[i.end] = NULL;
+	return (envp);
 }
