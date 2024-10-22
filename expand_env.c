ceeeb2a38f474c7833130d508ae4a5e252ec3c37
caqueiro <cassiusbessa@gmail.com>
2024-10-21 21:34:02 -0300

    normalize

--- expand_env.c
+++ expand_env.c
@@ -1,109 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_env.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caqueiro <caqueiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 21:29:00 by caqueiro          #+#    #+#             */
+/*   Updated: 2024/10/21 21:29:02 by caqueiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-static char *handle_null_env(t_part p, char *w);
-static char *expand_env(t_hashmap *envs, char *key, t_part p, char *w);
-static void expand_envs(t_hashmap *envs, t_token *t);
-static char *handle_start_with_number_key(char *key, t_part p, char *w);
+static char	*handle_null_env(t_part p, char *w);
+static char	*expand_env(t_hashmap *envs, char *key, t_part p, char *w);
+static void	expand_envs(t_hashmap *envs, t_token *t);
+static char	*handle_start_with_number_key(char *key, t_part p, char *w);
 
-void  expand_all_envs(t_main  *main)
+void	expand_all_envs(t_main *main)
 {
-  t_token *t;
+	t_token	*t;
 
-  t = main->token_lst->head;
-  while (t)
-  {
-    if (t->type == ARGUMMENT || t->type == COMMAND)
-      expand_envs(main->envs, t);
-    t = t->next;
-  }
+	t = main->token_lst->head;
+	while (t)
+	{
+		if (t->type == ARGUMMENT || t->type == COMMAND)
+			expand_envs(main->envs, t);
+		t = t->next;
+	}
 }
 
-static char *expand_env(t_hashmap *envs, char *key, t_part p, char *w)
+static char	*expand_env(t_hashmap *envs, char *key, t_part p, char *w)
 {
-    char    *value;
-    char    *expanded;
-    int     i;
-    int     j;
+	char	*value;
+	char	*expanded;
+	int		i;
+	int		j;
 
-    if (ft_isdigit(key[0]))
-        return (handle_start_with_number_key(key, p, w));
-    value = get_value(envs, key);
-    if (!value)
-        return (handle_null_env(p, w));
-    expanded = (char *)ft_calloc(ft_strlen(w) - (p.end - p.start) + ft_strlen(value), sizeof(char));
-    j = 0;
-    i = 0;
-    while (i < p.start - 1)
-    {
-        expanded[i] = w[i];
-        i++;
-    }
-    while (value[j])
-        expanded[i++] = value[j++];
-    while (w[p.end])
-        expanded[i++] = w[p.end++];
-    expanded[i] = '\0';
-    return (expanded);
+	if (ft_isdigit(key[0]))
+		return (handle_start_with_number_key(key, p, w));
+	value = get_value(envs, key);
+	if (!value)
+		return (handle_null_env(p, w));
+	expanded = (char *)ft_calloc(ft_strlen(w) - (p.end - p.start)
+			+ ft_strlen(value), sizeof(char));
+	j = 0;
+	i = 0;
+	while (i < p.start - 1)
+	{
+		expanded[i] = w[i];
+		i++;
+	}
+	while (value[j])
+		expanded[i++] = value[j++];
+	while (w[p.end])
+		expanded[i++] = w[p.end++];
+	expanded[i] = '\0';
+	return (expanded);
 }
 
-static void  expand_envs(t_hashmap *envs, t_token *t)
+static void	expand_envs(t_hashmap *envs, t_token *t)
 {
+	char	*expanded;
+	t_part	p;
+	char	*key;
 
-  char  *expanded;
-  t_part  p;
-  char  *key;
-
-  p = find_env(t->word);
-  if (p.start == p.end)
-    return ;
-  key = get_env_key(t->word, p);
-  while (p.start != p.end)
-  {
-    expanded = expand_env(envs, key, p, t->word);
-    free(key);
-    free(t->word);
-    t->word = expanded;
-    p = find_env(t->word);
-    if (p.start == p.end)
-      break ;
-    key = get_env_key(t->word, p);
-  }
+	p = find_env(t->word);
+	if (p.start == p.end)
+		return ;
+	key = get_env_key(t->word, p);
+	while (p.start != p.end)
+	{
+		expanded = expand_env(envs, key, p, t->word);
+		free(key);
+		free(t->word);
+		t->word = expanded;
+		p = find_env(t->word);
+		if (p.start == p.end)
+			break ;
+		key = get_env_key(t->word, p);
+	}
 }
 
-static char  *handle_null_env(t_part p, char *w)
+static char	*handle_null_env(t_part p, char *w)
 {
-  char  *expanded;
-  int   i;
+	char	*expanded;
+	int		i;
 
-  expanded = (char *)ft_calloc(ft_strlen(w) - (p.end - p.start), sizeof(char));
-  i = 0;
-  while (i < p.start - 1)
-  {
-    expanded[i] = w[i];
-    i++;
-  }
-  while (w[p.end])
-    expanded[i++] = w[p.end++];
-  expanded[i] = '\0';
-  return (expanded);
+	expanded = (char *)ft_calloc(ft_strlen(w) - (p.end - p.start),
+			sizeof(char));
+	i = 0;
+	while (i < p.start - 1)
+	{
+		expanded[i] = w[i];
+		i++;
+	}
+	while (w[p.end])
+		expanded[i++] = w[p.end++];
+	expanded[i] = '\0';
+	return (expanded);
 }
 
-static char *handle_start_with_number_key(char *key, t_part p, char *w)
+static char	*handle_start_with_number_key(char *key, t_part p, char *w)
 {
-    char    *expanded;
-    int     i;
-    int     j;
+	char	*expanded;
+	int		i;
+	int		j;
 
-    expanded = (char *)ft_calloc(ft_strlen(w) - 1, sizeof(char));
-    i = 0;
-    while (i < p.start - 1)
-    {
-        expanded[i] = w[i];
-        i++;
-    }
-    j = p.start + 1;
-    while (w[j])
-        expanded[i++] = w[j++];
-    expanded[i] = '\0';
-    return expanded;
+	expanded = (char *)ft_calloc(ft_strlen(w) - 1, sizeof(char));
+	i = 0;
+	while (i < p.start - 1)
+	{
+		expanded[i] = w[i];
+		i++;
+	}
+	j = p.start + 1;
+	while (w[j])
+		expanded[i++] = w[j++];
+	expanded[i] = '\0';
+	return (expanded);
 }
--- get_env_key.c
+++ get_env_key.c
@@ -1,73 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_env_key.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caqueiro <caqueiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 21:29:26 by caqueiro          #+#    #+#             */
+/*   Updated: 2024/10/21 21:29:47 by caqueiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-
-// 1 - percorrer string procurando $ sem aspas simples aberta;
-// 2 - puxar valor da env e pegar o seu tamanho e soma com o len total do input;
-// 3 - mallocar de novo com o tamanho total
-// 4 - copiar mudando o valor
-
-static int handle_quotes(char c, int quotes)
+static int	handle_quotes(char c, int quotes)
 {
-    if (c == '\'' && quotes == 0)
-        return c;
-    if (c == quotes)
-        return 0;
-    return quotes;
+	if (c == '\'' && quotes == 0)
+		return (c);
+	if (c == quotes)
+		return (0);
+	return (quotes);
 }
 
-static t_part find_variable_brackets(char *w, int *i)
+static t_part	find_variable_brackets(char *w, int *i)
 {
-    t_part p;
+	t_part	p;
 
-    (*i)++;
-    p = (t_part){.start = *i, .end = 0};
-    while (w[*i] && w[*i] != '}')
-        (*i)++;
-    p.end = *i;
-    return p;
+	(*i)++;
+	p = (t_part){.start = *i, .end = 0};
+	while (w[*i] && w[*i] != '}')
+		(*i)++;
+	p.end = *i;
+	return (p);
 }
 
-static t_part find_variable_name(char *w, int *i)
+static t_part	find_variable_name(char *w, int *i)
 {
-    t_part p;
+	t_part	p;
 
-    p = (t_part){.start = *i, .end = 0};
-    while (w[*i] && (isalnum((int)w[*i]) || w[*i] == '_'))
-        (*i)++;
-    p.end = *i;
-    return p;
+	p = (t_part){.start = *i, .end = 0};
+	while (w[*i] && (isalnum((int)w[*i]) || w[*i] == '_'))
+		(*i)++;
+	p.end = *i;
+	return (p);
 }
 
-t_part find_env(char *w)
+t_part	find_env(char *w)
 {
-    int i;
-    int quotes;
-    t_part p;
+	int		i;
+	int		quotes;
+	t_part	p;
 
-    quotes = 0;
-    p = (t_part){0};
-    i = 0;
-    while (w[i])
-    {
-        quotes = handle_quotes(w[i], quotes);
-        if (w[i] == '$' && !quotes)
-        {
-					i++;
-					if (w[i] == '?')
-						return (t_part){.start = i, .end = i + 1};
-					if (ft_isdigit(w[i]))
-						return (p.start = i, p.end = -1, p);
-					if (w[i] == '{')
-						return find_variable_brackets(w, &i);
-					else if (ft_isalpha(w[i]) || w[i] == '_')
-						return find_variable_name(w, &i);
-        }
-        i++;
-    }
-    return p;
+	quotes = 0;
+	p = (t_part){0};
+	i = 0;
+	while (w[i])
+	{
+		quotes = handle_quotes(w[i], quotes);
+		if (w[i] == '$' && !quotes)
+		{
+			i++;
+			if (w[i] == '?')
+				return ((t_part){.start = i, .end = i + 1});
+			if (ft_isdigit(w[i]))
+				return (p.start = i, p.end = -1, p);
+			if (w[i] == '{')
+				return (find_variable_brackets(w, &i));
+			else if (ft_isalpha(w[i]) || w[i] == '_')
+				return (find_variable_name(w, &i));
+		}
+		i++;
+	}
+	return (p);
 }
 
-char    *get_env_key(char *w, t_part p)
+char	*get_env_key(char *w, t_part p)
 {
-    return (ft_substr(w, p.start, p.end - p.start));
+	return (ft_substr(w, p.start, p.end - p.start));
 }
